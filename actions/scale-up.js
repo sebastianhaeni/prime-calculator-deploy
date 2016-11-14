e6d016997b6ad25f6b392a5d308fa9bb8a6c6143
Sebastian Häni <haeni.sebastian@gmail.com>
2016-11-15 00:54:39 +0100

    added check call

---
+++ actions/scale-up.js
@@ -0,0 +1,4 @@
+
+module.exports = function(droplets){
+    console.log('Scaling up');
+};
--- tasks/status-check.js
+++ tasks/status-check.js
@@ -1,6 +1,26 @@
+const remoteSSH = require('../actions/remote-ssh');
+const scaleUp = require('../actions/scale-up');
+const scaleDown = require('../actions/scale-down');
 
 function checkStatus() {
-    console.log('checking status on nodes');
+    console.log('\n\nChecking status on nodes');
+
+    getDroplets('lamp').then(droplets => {
+        let totalUsage = droplets.map(droplet => {
+            let ip = droplet.networks.v4.find(network => network.type === 'public').ip_address;
+            let cpuUsage = remoteSSH(`top -bn 2 -d 2 | grep '^%Cpu' | tail -n 1 | gawk '{print $2+$4+$6}'`, ip);
+            console.log(`${ip} has a usage of ${cpuUsage}%`);
+            return cpuUsage;
+        }).reduce((a, b)=>a + b, 0);
+
+        let average = totalUsage / droplets.length;
+
+        if (average > 50) {
+            scaleUp(droplets);
+        } else if (average < 5) {
+            scaleDown(droplets);
+        }
+    }).then(() => setTimeout(checkStatus, 5000));
 }
 
 setTimeout(checkStatus, 5000);
