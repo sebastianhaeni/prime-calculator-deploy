43c5c222e3946d9d235b513397e2a3b3cb40a976
Sebastian Häni <haeni.sebastian@gmail.com>
2016-11-14 23:53:08 +0100

    fix remote ssh

--- actions/remote-ssh.js
+++ actions/remote-ssh.js
@@ -1,5 +1,5 @@
 const childProcess = require('child_process');
 
 module.exports = function (command, ip, options) {
-    return childProcess.execSync(`ssh root@${ip} '${command}`, options);
+    return childProcess.execSync(`ssh root@${ip} '${command}'`, options);
 };
