8abce34e3f29b1e4eded1b78f998bf6761ea3a91
Sebastian Häni <haeni.sebastian@gmail.com>
2016-11-21 19:44:00 +0100

    remove debug logs

--- actions/update-haproxy-config.js
+++ actions/update-haproxy-config.js
@@ -3,9 +3,7 @@ const path = require('path');
 const config = require('../config');
 
 function updateHAProxyConfig(lamps) {
-    console.log(lamps);
     let proxyConfig = fs.readFileSync(path.join(__dirname, '../resources/haproxy.template.cfg'), 'utf8');
-    console.log(proxyConfig);
     proxyConfig = proxyConfig.replace('${statsPassword}', config.STATS_PASSWORD);
 
     let re = new RegExp('{repeat}([a-zA-Z0-9\\${}:\\W]*){endRepeat}');
@@ -20,7 +18,6 @@ function updateHAProxyConfig(lamps) {
     });
 
     proxyConfig = proxyConfig.replace(re, servers.join(''));
-    console.log(proxyConfig);
 
     fs.writeFileSync(path.join(__dirname, '../stage/haproxy.cfg'), proxyConfig, {flag: 'w'});
 }
