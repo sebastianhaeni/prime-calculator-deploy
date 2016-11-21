95f9923c52e58cc35a40c8bc4108af82381fe94e
Sebastian Häni <haeni.sebastian@gmail.com>
2016-11-21 19:39:24 +0100

    logging

--- actions/update-haproxy-config.js
+++ actions/update-haproxy-config.js
@@ -4,25 +4,25 @@ const config = require('../config');
 
 function updateHAProxyConfig(lamps) {
     console.log(lamps);
-    return fs.readFile(path.join(__dirname, '../resources/haproxy.template.cfg'), 'utf8', function (err, proxyConfig) {
-        proxyConfig = proxyConfig.replace('${statsPassword}', config.STATS_PASSWORD);
+    let proxyConfig = fs.readFileSync(path.join(__dirname, '../resources/haproxy.template.cfg'), 'utf8');
+    console.log(proxyConfig);
+    proxyConfig = proxyConfig.replace('${statsPassword}', config.STATS_PASSWORD);
 
-        let re = new RegExp('{repeat}([a-zA-Z0-9\\${}:\\W]*){endRepeat}');
-        let serverStringTemplate = re.exec(proxyConfig)[1];
+    let re = new RegExp('{repeat}([a-zA-Z0-9\\${}:\\W]*){endRepeat}');
+    let serverStringTemplate = re.exec(proxyConfig)[1];
 
-        let servers = [];
+    let servers = [];
 
-        lamps.forEach(lamp => {
-            let serverString = serverStringTemplate.replace('${serverName}', lamp.name);
-            serverString = serverString.replace('${serverIp}', lamp.ip);
-            servers.push(serverString);
-        });
-
-        proxyConfig = proxyConfig.replace(re, servers.join(''));
-        console.log(proxyConfig);
-
-        fs.writeFileSync(path.join(__dirname, '../stage/haproxy.cfg'), proxyConfig, {flag: 'w'});
+    lamps.forEach(lamp => {
+        let serverString = serverStringTemplate.replace('${serverName}', lamp.name);
+        serverString = serverString.replace('${serverIp}', lamp.ip);
+        servers.push(serverString);
     });
+
+    proxyConfig = proxyConfig.replace(re, servers.join(''));
+    console.log(proxyConfig);
+
+    fs.writeFileSync(path.join(__dirname, '../stage/haproxy.cfg'), proxyConfig, {flag: 'w'});
 }
 
 module.exports = updateHAProxyConfig;
